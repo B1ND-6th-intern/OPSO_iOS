fe587f21e87be04f2dc029812d85e5605501a0fb
SangAu124 <81676542+SangAu124@users.noreply.github.com>
2021-08-03 18:56:23 +0900

    OPSO_iOS_Storyboard
    스토리보드 조금 만져보기

---
+++ OPSO_Login/OPSO_Login/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  OPSO_Login
+//
+//  Created by DGSW on 2021/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var IDtextField: UITextField!
+    @IBOutlet var PWtextField: UITextField!
+    @IBOutlet var LoginButton: UIButton!
+    @IBOutlet var MoveToSignUp: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func Click_LoginBtn(_ sender: Any) {
+        
+    }
+    
+}
+
